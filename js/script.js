9b0bd03a2b0350439ff3c339c3e527eeee7bdb40
Łukasz Mróz <lmroz95@gmail.com>
2022-03-20 11:42:00 +0100

    Add second part of the code to the script.js file

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ if (randomNumber == '1') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+// Ustalenie ruchu gracza
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyczki.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyczki';
+} else {
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
